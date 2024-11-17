2b4b186fb1dd10eee6a39314fbef7d774d6cf129
hizellal <vladimir.liubshenko@gmail.com>
2024-11-17 19:36:23 +0200

    final.comm

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    let list = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            list.push(numbers[i]);
+        }
+    }
+    return list;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
